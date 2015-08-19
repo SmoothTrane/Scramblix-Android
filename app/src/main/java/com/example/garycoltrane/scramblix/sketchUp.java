dceb7615288433f7dd615345caabd01e88e17a47
GColtrane <Coltranegary@gmail.com>
2015-08-18 22:01:47 -0400

    Edited the x positions, need to make sure x coordinates with every value in arr array

--- app/src/main/java/com/example/garycoltrane/scramblix/sketchUp.java
+++ app/src/main/java/com/example/garycoltrane/scramblix/sketchUp.java
@@ -15,21 +15,24 @@ import android.view.SurfaceView;
 /**
  * Created by garycoltrane on 8/11/15.
  */
-public class sketchUp extends View {
+public class sketchUp extends View  {
     int firstCharacterY;
-int x=0;
-    int xArray [];
+int x = (int) getRandomX();
+    int[] xArray= getXArray();
+
     int y=0;
     double lastCharacterY=0;
     String check;
     Rect disRect;
+    Canvas canvas;
     public sketchUp(Context context) {
         super(context);
 
     }
-String  [] arr = {"Dedic","love"};
+String [] arr = {"Dedic","love","happiness"};
     protected void onDraw (Canvas canvas) {
         super.onDraw(canvas);
+
         disRect = new Rect();
         disRect.set(0, 0, canvas.getWidth(), canvas.getHeight() / 2);
         Paint col = new Paint();
@@ -39,73 +42,78 @@ String  [] arr = {"Dedic","love"};
         col.setColor(Color.GREEN);
         col.setStyle(Paint.Style.FILL);
         canvas.drawRect(disRect, col);
-invalidate();
 
-//       if(x<canvas.getWidth()){
-//            x+=10;
-//        }
-//        else{
-//           x=0;
-//       }
-
-        for (int i = 0; i < arr.length; i++) {
-            x= (int) getRandomX();
-            if (y < canvas.getHeight() / 2) {
-                y += 2;
+            for (int i = 0; i < arr.length; i++) {
+                if (y < canvas.getHeight() / 2) {
+                    y += 2;
 
 
-            }
-            if (y > canvas.getHeight() / 2) {
-                y = 0;
-                x = (int) getRandomX();
+                }
+                if (y > canvas.getHeight() / 2) {
+                    y = 0;
+                    x = (int) getRandomX();
 
-            }
+                }
+
 
 //      if(lastCharacterY>canvas.getHeight()/2){
 //          y=0;
 //      }
-            for (int j = 0; j < arr[i].length(); j++) {
-                char result = arr[i].charAt(j);
-                check = String.valueOf(result);
 
-                canvas.drawText(check, x, y + 40 * j, txt);
-                Log.v("Activity", check);
+                for (int j = 0; j < arr[i].length(); j++) {
+                    char result = arr[i].charAt(j);
+                    check = String.valueOf(result);
+
+                    canvas.drawText(check, xArray[i], y + 40 * j, txt);
 
 //        }
 
-                // }
+                    // }
 
 
-            }
+                }
 
-            if (y < canvas.getHeight() / 2) {
+                if (y < canvas.getHeight() / 2) {
 
-                lastCharacterY += arr[i].length() / 1.6;
+                    lastCharacterY += arr[i].length() / 1.6;
 
 
-            }
-            if (lastCharacterY > canvas.getHeight() / 2) {
-                y = 0;
-                lastCharacterY = arr[i].length() / 1.6;
-                x = (int) getRandomX();
-            }
+                }
+                if (lastCharacterY > canvas.getHeight() / 2) {
+                    y = 0;
+                    lastCharacterY = arr[i].length() / 1.6;
+                    x = (int) getRandomX();
+                }
 
-//      Log.d("Activity",Integer.toString(y));
+//     Log.d("Activity",Integer.toString(y));
 //           Log.v("Activity",Double.toString(lastCharacterY));
 
+                invalidate();
 
+            }
         }
-    }
 
 
 
     public  double getRandomX(){
 
-        double X = (Math.random()*disRect.width()+1 -0) +0;
-
-        //(disRect.width+1 - 0) + 0
+        double X = (Math.random()*super.getWidth() -0) +0;
 return X;
     }
+
+public int[] getXArray(){
+    xArray= new int[3];
+   for(int i=0; i<xArray.length; i++){
+
+       xArray[i] = x;
+      // Log.v("Activity", Integer.toString(super.getWidth()));
+
+
+   }
+return xArray;
+}
+
+
 }
 
 
