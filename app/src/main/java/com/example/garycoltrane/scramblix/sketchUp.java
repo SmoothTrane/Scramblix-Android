b64ffca3293ca23fc781dcd33a7ce86ca104e04e
GColtrane <Coltranegary@gmail.com>
2015-08-25 04:02:47 -0400

    Fixed issue in regards of all elements starting at 0,0,0

--- app/src/main/java/com/example/garycoltrane/scramblix/sketchUp.java
+++ app/src/main/java/com/example/garycoltrane/scramblix/sketchUp.java
@@ -9,31 +9,42 @@ import android.graphics.Rect;
 import android.graphics.Paint;
 import android.widget.Toast;
 import android.util.Log;
+
 import java.util.*;
+
 import android.view.SurfaceView;
+
 import static java.lang.Thread.State;
 
 /**
  * Created by garycoltrane on 8/11/15.
  */
-public class sketchUp extends View  {
+public class sketchUp extends View {
+
     int firstCharacterY;
-int x = (int) getRandomX();
-    int[] xArray= getXArray();
-LinkedList <Integer> disList = new LinkedList<Integer>();
-    int y=0;
-    double lastCharacterY=0;
+    int x = (int) getRandomX();
+   // int[] xArray = getXArray();
+    LinkedList<Integer> disList = new LinkedList<Integer>();
+    String[] arr = {"Dedic", "love", "happiness"};
+    int canvasWidth= super.getWidth();
+    Integer[] a = ListOf();
+    int y = 0;
+    double lastCharacterY = 0;
     String check;
     Rect disRect;
     Canvas canvas;
+int count =0;
     public sketchUp(Context context) {
         super(context);
 
-    }
-String [] arr = {"Dedic","love","happiness"};
-    protected void onDraw (Canvas canvas) {
-        super.onDraw(canvas);
 
+    }
+
+
+    protected void onDraw(Canvas canvas) {
+
+int l= super.getWidth();
+        super.onDraw(canvas);
         disRect = new Rect();
         disRect.set(0, 0, canvas.getWidth(), canvas.getHeight() / 2);
         Paint col = new Paint();
@@ -44,81 +55,99 @@ String [] arr = {"Dedic","love","happiness"};
         col.setStyle(Paint.Style.FILL);
         canvas.drawRect(disRect, col);
 
-            for (int i = 0; i < arr.length; i++) {
-                if (y < canvas.getHeight() / 2) {
-                    y += 2;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (y < canvas.getHeight() / 2) {
+                y += 1;
 
 
-                }
-                if (y > canvas.getHeight() / 2) {
-                    y = 0;
-                    x = (int) getRandomX();
+            }
+            if (y > canvas.getHeight() / 2) {
+                y = 0;
+                //x = (int) getRandomX();
 
-                }
-
-                for (int j = 0; j < arr[i].length(); j++) {
-                    char result = arr[i].charAt(j);
-                    check = String.valueOf(result);
+            }
 
 
-             canvas.drawText(check,disList.get(i), y + 40 * j, txt);
+            for (int j = 0; j < arr[i].length(); j++) {
+                char result = arr[i].charAt(j);
+                check = String.valueOf(result);
+
+if(a[i]==0){
+    ListOf();
+}
+                canvas.drawText(check, a[i], y + 40 * j, txt);
+
+
 // ;
 //        }
 
-                    // }
+                // }
 
-                }
+            }
 
-                if (y < canvas.getHeight() / 2) {
-                    lastCharacterY += arr[i].length() / 1.6;
+            if (y < canvas.getHeight() / 2) {
+                lastCharacterY += arr[i].length() / 1.6;
 
 
-                }
-                if (lastCharacterY > canvas.getHeight() / 2) {
-                    ListOf();
-                    y = 0;
-                    lastCharacterY = arr[i].length() / 1.6;
+            }
 
-                }
 
-//     Log.d("Activity",Integer.toString(y));
-//           Log.v("Activity",Double.toString(lastCharacterY));
-
-                invalidate();
+            if (lastCharacterY > canvas.getHeight() / 2) {
+                ListOf();
+                y = 0;
+                lastCharacterY = arr[i].length() / 1.6;
 
             }
         }
+//     Log.d("Activity",Integer.toString(y));
+//           Log.v("Activity",Double.toString(lastCharacterY));
 
-    public  double getRandomX(){
+            invalidate();
+        }
 
-        int X =(int) Math.floor((Math.random() * (super.getWidth() - 0+1)) +0);
+
+    public double getRandomX() {
+
+        int X = (int) Math.floor((Math.random() * (super.getWidth() - 0 + 1)) + 0);
 
 
         return X;
     }
-public int getNum(){
 
-    return 500;
-}
-public int[] getXArray(){
-   int [] bArray= new int[3];
-   for(int i=0; i<bArray.length; i++){
+    public int getNum() {
 
-       bArray[i] = (int) getRandomX();
+        return 500;
+    }
 
+    public int[] getXArray() {
+        int[] bArray = new int[3];
+        for (int i = 0; i < bArray.length; i++) {
 
-
-   }
-return bArray;
-}
-public Integer[] ListOf () {
-    disList.addFirst((int) getRandomX());
-return disList.toArray();
-
-//disList.
+            bArray[i] = (int) getRandomX();
 
 
-}
+        }
+        return bArray;
+    }
+
+    public Integer[] ListOf() {
+
+            for (int i = 0; i < arr.length; i++) {
+                disList.push((int) getRandomX());
+            }
+            a = disList.toArray(new Integer[disList.size()]);
+
+
+
+
+
+
+
+
+            return a;
+    }
+
 }
 
 
